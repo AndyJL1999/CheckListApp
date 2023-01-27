b36fc54153638b013042dec969c7ccde726172b8
AndyJL1999 <andylainez115@hotmail.com>
2023-01-26 19:31:36 -0500

    --Created models for tasks

---
+++ CheckListApi/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CheckListApi.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [MaxLength(25)]
+        public string Title { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        public bool InProgress { get; set; } = false;
+        public bool IsDone { get; set; } = false;
+    }
+}
---
+++ CheckListApi/Models/TaskBoard.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CheckListApi.Models
+{
+    public class TaskBoard
+    {
+        public int Id { get; set; }
+
+        [MaxLength(25)]
+        public string Title { get; set; }
+        public List<Task> Tasks { get; set; }
+    }
+}
